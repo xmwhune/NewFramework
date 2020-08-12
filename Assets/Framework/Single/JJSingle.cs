9eee6d573ef12ae18486753aedb1cdb9516142e5
xmwhune <xmwhune_509@sina.com>
2020-08-12 23:31:41 +0800

    第一版

---
+++ Assets/Framework/Single/JJSingle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace JJ
+{
+    public class JJSingle
+    {
+       private JJSingle(){}
+       protected JJSingle(type t):this()
+       {
+           mType = t;
+           mName = typeof(t).Name;
+       }
+       protected Type mType;
+       protected string mName;
+    }
+}
+
