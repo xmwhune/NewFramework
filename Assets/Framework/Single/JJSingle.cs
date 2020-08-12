96cf6e0c87649838e4f712930e236bff8a747531
xmwhune <xmwhune_509@sina.com>
2020-08-13 00:10:20 +0800

    第一版v.0.0.1

--- Assets/Framework/Single/JJSingle.cs
+++ Assets/Framework/Single/JJSingle.cs
@@ -13,6 +13,15 @@ namespace JJ
        }
        protected Type mType;
        protected string mName;
+       public virtual void OnAwake(){}
+       public virtual void OnActive(){}
+       public virtual void OnUpdate(){}
+       public virtual void OnLateUpdate(){}
+       public virtual void OnPause(){}
+       public virtual void OnContinue(){}
+       public virtual void  OnDisable(){}
+       public virtual void OnAppQuit(){}
+       public virtual void OnDestroy(){}
     }
 }
 
